4674b50f6b8f433b03e4db0055840133edd2e95c
Henrique Cezar <hcgv1@hotmail.com>
2026-03-25 15:58:07 -0300

    creating BatchDTO

---
+++ noWaste/src/main/java/A3/project/noWaste/dto/BatchDTO.java
@@ -0,0 +1,37 @@
+package A3.project.noWaste.dto;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchDTO {
+
+    private Integer id;
+
+    @NotBlank(message = "O codigo do lote e obrigatorio")
+    private String code;
+
+    @NotNull(message = "A quantidade e obrigatoria")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero")
+    private Integer quantity;
+
+    @NotNull(message = "A data de validade e obrigatoria")
+    @FutureOrPresent(message = "A data de validade nao pode ser no passado")
+    private LocalDate expirationDate;
+
+    private Double totalWeight;
+
+    @NotNull(message = "O produto e obrigatorio")
+    private Integer productId;
+}
