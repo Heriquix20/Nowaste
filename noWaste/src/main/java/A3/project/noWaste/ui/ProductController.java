acbb9930ec75703bdf2599f19c5cbc70f96769b4
Henrique Cezar <hcgv1@hotmail.com>
2026-03-25 15:56:26 -0300

    modifying and establishing the product controller class

--- noWaste/src/main/java/A3/project/noWaste/ui/ProductController.java
+++ noWaste/src/main/java/A3/project/noWaste/ui/ProductController.java
@@ -3,8 +3,8 @@ package A3.project.noWaste.ui;
 import A3.project.noWaste.domain.Product;
 import A3.project.noWaste.dto.ProductDTO;
 import A3.project.noWaste.service.ProductService;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,52 +15,54 @@ import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/products")
 public class ProductController {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+    private final ModelMapper mapper;
 
-    @Autowired
-    private ModelMapper mapper;
+    public ProductController(ProductService service, ModelMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
 
+    @GetMapping("/inventories/{inventoryId}/products")
+    public ResponseEntity<List<ProductDTO>> findAllByInventory(@PathVariable Integer inventoryId) {
+        List<Product> list = service.findAllByInventory(inventoryId);
+        List<ProductDTO> listDTO = list.stream()
+                .map(product -> mapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList());
 
-    // Buscar produto por ID
-    @GetMapping("/{id}")
+        return ResponseEntity.ok(listDTO);
+    }
+
+    @GetMapping("/products/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Integer id) {
         Product product = service.findById(id);
-        return ResponseEntity.ok().body(mapper.map(product, ProductDTO.class));
+        return ResponseEntity.ok(mapper.map(product, ProductDTO.class));
     }
 
-    // Listar todos os produtos
-    @GetMapping
-    public ResponseEntity<List<ProductDTO>> findAll() {
-        List<Product> list = service.findAll();
-        List<ProductDTO> listDTO = list.stream()
-                .map(p -> mapper.map(p, ProductDTO.class))
-                .collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDTO);
-    }
+    @PostMapping("/inventories/{inventoryId}/products")
+    public ResponseEntity<ProductDTO> create(
+            @PathVariable Integer inventoryId,
+            @Valid @RequestBody ProductDTO obj
+    ) {
+        Product newProduct = service.create(inventoryId, obj);
 
-    // Criar produto
-    @PostMapping
-    public ResponseEntity<ProductDTO> create(@RequestBody ProductDTO obj) {
-        Product newObj = service.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}").buildAndExpand(newObj.getId()).toUri();
-        return ResponseEntity.created(uri).build();
+                .path("/{id}")
+                .buildAndExpand(newProduct.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(mapper.map(newProduct, ProductDTO.class));
     }
 
-    // Atualizar produto
-    @PutMapping("/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Integer id, @RequestBody ProductDTO obj) {
-        obj.setId(id);
-        Product updated = service.update(obj);
-        return ResponseEntity.ok().body(mapper.map(updated, ProductDTO.class));
+    @PutMapping("/products/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Integer id, @Valid @RequestBody ProductDTO obj) {
+        Product updated = service.update(id, obj);
+        return ResponseEntity.ok(mapper.map(updated, ProductDTO.class));
     }
 
-    // Deletar produto
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
