26f518e0ae5d7744722345a48b6b1a0a86d6159b
Henrique Cezar <hcgv1@hotmail.com>
2026-04-02 11:42:09 -0300

    Updating the inventory to show creation data

--- noWaste/src/main/java/A3/project/noWaste/dto/InventoryDTO.java
+++ noWaste/src/main/java/A3/project/noWaste/dto/InventoryDTO.java
@@ -1,10 +1,13 @@
 package A3.project.noWaste.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,4 +23,7 @@ public class InventoryDTO {
     private String description;
 
     private String location;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime createdAt;
 }
