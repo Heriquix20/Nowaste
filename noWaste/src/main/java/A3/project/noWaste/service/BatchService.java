9d2699cee4cbd573782799106d2be915785d820f
Henrique Cezar <hcgv1@hotmail.com>
2026-04-10 17:14:43 -0300

    adding new quantity parameters to the listing method

--- noWaste/src/main/java/A3/project/noWaste/service/BatchService.java
+++ noWaste/src/main/java/A3/project/noWaste/service/BatchService.java
@@ -18,6 +18,8 @@ public interface BatchService {
             String status,
             LocalDate expirationFrom,
             LocalDate expirationTo,
+            Integer minQuantity,
+            Integer maxQuantity,
             String sortExpiration
     );
 
