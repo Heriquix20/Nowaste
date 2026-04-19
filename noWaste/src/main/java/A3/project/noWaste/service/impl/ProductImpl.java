fc80ed8a521720f251d10d2b19880c36ed5622db
Henrique <hcgv1@hotmail.com>
2026-04-19 18:44:45 -0300

    deleting dev-tools code example

--- noWaste/src/main/java/A3/project/noWaste/service/impl/ProductImpl.java
+++ noWaste/src/main/java/A3/project/noWaste/service/impl/ProductImpl.java
@@ -122,8 +122,6 @@ public class ProductImpl implements ProductService {
         product.setBrand(obj.getBrand());
         product.setWeightInGrams(convertToGrams(obj.getWeight(), obj.getWeightUnit()));
 
-        int a = 20;
-
         return repository.save(product);
     }
 
